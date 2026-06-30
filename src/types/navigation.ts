154025a6ac29d5c52b6c5e5423378a4921f7118d
awdh03280-max <awdh03280@gmail.com>
2026-06-30 09:00:10 +0300

    Add App.tsx and navigation types

---
+++ src/types/navigation.ts
@@ -0,0 +1,8 @@
+export type RootStackParamList = {
+  Home: undefined;
+  Search: undefined;
+  Favorites: undefined;
+  ContinueWatching: undefined;
+  MovieDetails: { movieId: string };
+  MovieList: { category: string };
+};
